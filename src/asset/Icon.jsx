05c77eadb75fa77031b86f92d9e3320204fa96e4
saerom <saerominew@gmail.com>
2023-11-08 14:04:17 +0900

    Update Icon.jsx

--- src/asset/Icon.jsx
+++ src/asset/Icon.jsx
@@ -1,29 +1,10 @@
-export const UserCircle = () => {
+export const LoginIcon = () => {
   return (
     <svg
       xmlns="http://www.w3.org/2000/svg"
-      width="50"
-      height="50"
-      fill="#D9D9D9"
-      class="bi bi-person-circle"
-      viewBox="0 0 16 16"
-    >
-      <path d="M11 6a3 3 0 1 1-6 0 3 3 0 0 1 6 0z" />
-      <path
-        fillRule="evenodd"
-        d="M0 8a8 8 0 1 1 16 0A8 8 0 0 1 0 8zm8-7a7 7 0 0 0-5.468 11.37C3.242 11.226 4.805 10 8 10s4.757 1.225 5.468 2.37A7 7 0 0 0 8 1z"
-      />
-    </svg>
-  );
-};
-
-export const User = () => {
-  return (
-    <svg
-      xmlns="http://www.w3.org/2000/svg"
-      width="16"
-      height="16"
-      fill="#808080"
+      width="40"
+      height="40"
+      fill="black"
       class="bi bi-person-fill"
       viewBox="0 0 16 16"
     >
@@ -32,17 +13,26 @@ export const User = () => {
   );
 };
 
-export const Lock = () => {
+export const PasswordIcon = () => {
   return (
     <svg
       xmlns="http://www.w3.org/2000/svg"
-      width="16"
-      height="16"
-      fill="#808080"
+      width="40"
+      height="40"
+      fill="black"
       class="bi bi-lock-fill"
       viewBox="0 0 16 16"
     >
       <path d="M8 1a2 2 0 0 1 2 2v4H6V3a2 2 0 0 1 2-2zm3 6V3a3 3 0 0 0-6 0v4a2 2 0 0 0-2 2v5a2 2 0 0 0 2 2h6a2 2 0 0 0 2-2V9a2 2 0 0 0-2-2z" />
     </svg>
+  );
+};
+
+export const BGSheet = () => {
+  return (
+    <link
+      href="https://fonts.googleapis.com/css2?family=Montserrat:wght@400;700;800&display=swap"
+      rel="stylesheet"
+    />
   );
 };
