ddb7df9e4f09bc4100811b4765e68f32dbcc9dcb
jaemnii <22jaemnii@gmail.com>
2023-11-15 14:47:21 +0900

    dd
    dd

--- src/components/Navbar/SidebarData.jsx
+++ src/components/Navbar/SidebarData.jsx
@@ -1,25 +1,25 @@
-import React, { useState } from 'react';
-import * as FaIcons from 'react-icons/fa';
-import * as AiIcons from 'react-icons/ai';
-import * as IoIcons from 'react-icons/io';
+import React, { useState } from "react";
+import * as FaIcons from "react-icons/fa";
+import * as AiIcons from "react-icons/ai";
+import * as IoIcons from "react-icons/io";
 
 export const SidebarData = [
   {
-    title: '공지사항',
+    title: "공지사항",
     icon: <IoIcons.IoIosPaper />,
-    cName: 'nav-button',
+    cName: "nav-button",
     modal: true,
   },
   {
-    title: '나의 예약정보',
+    title: "나의 예약정보",
     icon: <FaIcons.FaCartPlus />,
-    cName: 'nav-button',
+    cName: "nav-button",
     modal: true,
   },
   {
-    title: '로그아웃',
-    path: '/',
+    title: "로그아웃",
+    path: "/",
     icon: <AiIcons.AiFillHome />,
-    cName: 'nav-button',
+    cName: "nav-button",
   },
 ];
